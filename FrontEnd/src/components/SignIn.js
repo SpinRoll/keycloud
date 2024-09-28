99eceb2a99002047d945c657856b0d2369b27f8b
IonCatana <catana.ion17@yahoo.it>
2024-09-28 16:56:01 +0200

    Refactor SignIn.js to use new SignInForm component

--- FrontEnd/src/components/SignIn.js
+++ FrontEnd/src/components/SignIn.js
@@ -3,63 +3,80 @@ import React, { useContext, useState } from "react";
 import { Container, Box, Typography, Link, IconButton } from "@mui/material";
 import { Brightness4, Brightness7 } from "@mui/icons-material";
 import Logo from "./logo/Logo";
-import CustomTextField from "./customComponents/CustomTextField";
-import CustomButton from "./customComponents/CustomButton";
+import SignInForm from "./forms/auth/SignInForm"; // Importa il nuovo form
 import { useTheme } from "@mui/material/styles";
 import { pxToRem } from "../utils/pxToRem";
 import { ThemeContext } from "../context/ThemeContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useTranslation } from "react-i18next"; // Hook per le traduzioni
+import InfoModal from "./modals/InfoModal";
 
 function SignIn() {
   const theme = useTheme();
   const { toggleTheme, mode } = useContext(ThemeContext);
   const navigate = useNavigate();
   const { t } = useTranslation(); // Uso delle traduzioni
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [userName, setUserName] = useState(""); // Nome dell'utente
+  const [userSurname, setUserSurname] = useState(""); // Cognome dell'utente
 
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    mfaToken: "", // Aggiungi un campo per il codice MFA
+    mfaToken: "",
   });
 
   const [error, setError] = useState(""); // Stato per gestire gli errori
   const [mfaRequired, setMfaRequired] = useState(false); // Stato per gestire se l'MFA è richiesto
 
-  // Funzione per gestire i cambiamenti degli input
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  // Funzione per gestire la richiesta di login
   const handleSignIn = async (e) => {
     e.preventDefault();
-    setError(""); // Resetta l'errore prima di inviare la richiesta
+    setError("");
 
     try {
       const response = await axios.post("/api/auth/signin", formData);
 
-      // Se MFA è richiesto, chiedi il codice MFA
       if (response.data.mfaRequired) {
         setMfaRequired(true);
-        return; // Fermiamo il flusso per chiedere il codice MFA
+        return;
       }
 
-      // Memorizza i token JWT nel localStorage
+      // Salva il nome e cognome dell'utente dalla risposta
+      const { nome, cognome } = response.data.result;
+      setUserName(nome);
+      setUserSurname(cognome);
+
+      // Mostra il messaggio di benvenuto
+      setSuccessMessage(t("signin_success"));
+      setIsModalOpen(true);
+
+      // Salva i token nel localStorage
       localStorage.setItem("token", response.data.accessToken);
       localStorage.setItem("refreshToken", response.data.refreshToken);
       localStorage.setItem("userId", response.data.result._id);
 
-      navigate("/dashboard"); // Naviga alla Dashboard dopo il successo
+      // Naviga alla dashboard dopo 2 secondi
+      setTimeout(() => {
+        handleCloseModal();
+      }, 2000);
     } catch (error) {
       setError(
         error.response ? error.response.data.message : t("signin_error")
       );
     }
   };
+  // Funzione per chiudere il modal
+  const handleCloseModal = () => {
+    setIsModalOpen(false); // Chiudi il modal
+    navigate("/dashboard"); // Naviga alla dashboard
+  };
 
-  // Funzione per navigare alla pagina di sign-up
   const handleSignUpClick = () => {
     navigate("/sign-up");
   };
@@ -84,92 +101,44 @@ function SignIn() {
         <Typography
           component="h2"
           variant="h6"
-          sx={{
-            color: theme.palette.text.primary,
-            textAlign: "center",
-          }}>
+          sx={{ color: theme.palette.text.primary }}>
           {t("signin_title")}
         </Typography>
 
-        {/* Form */}
-        <Box component="form" noValidate onSubmit={handleSignIn}>
-          <CustomTextField
-            label={t("email_label")}
-            name="email"
-            autoFocus
-            onChange={handleInputChange}
-          />
-          <CustomTextField
-            label={t("password_label")}
-            name="password"
-            type="password"
-            onChange={handleInputChange}
-          />
+        {/* Usa il nuovo componente SignInForm */}
+        <SignInForm
+          formData={formData}
+          handleInputChange={handleInputChange}
+          handleSignIn={handleSignIn}
+          error={error}
+          t={t}
+          mfaRequired={mfaRequired}
+        />
 
-          {/* Mostra il campo MFA solo se richiesto */}
-          {mfaRequired && (
-            <CustomTextField
-              label={t("mfa_code_label")}
-              name="mfaToken"
-              type="text"
-              onChange={handleInputChange}
-            />
-          )}
+        <IconButton onClick={toggleTheme} sx={{ mt: pxToRem(2) }}>
+          {mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
 
-          {/* Recover email */}
-          <Typography
-            component="p"
+        <Typography
+          component="p"
+          variant="body2"
+          sx={{ color: theme.palette.text.primary }}>
+          {t("no_account")}{" "}
+          <Link
             variant="body2"
-            sx={{
-              color: theme.palette.text.primary,
-              mt: pxToRem(8),
-              mb: pxToRem(16),
-            }}>
-            {t("forgot_password")}{" "}
-            <Link
-              variant="body2"
-              sx={{ color: theme.palette.primary.main, cursor: "pointer" }}
-              href="/recover-email">
-              {t("recover_password")}
-            </Link>
-          </Typography>
-
-          {error && (
-            <Typography color="error" variant="body2" sx={{ mt: 2 }}>
-              {error}
-            </Typography>
-          )}
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: pxToRem(16),
-              mt: pxToRem(8),
-            }}>
-            <CustomButton type="submit">{t("signin_button")}</CustomButton>
-          </Box>
-
-          <IconButton onClick={toggleTheme} sx={{ mt: pxToRem(2) }}>
-            {mode === "dark" ? <Brightness7 /> : <Brightness4 />}
-          </IconButton>
-
-          <Typography
-            component="p"
-            variant="body2"
-            sx={{
-              color: theme.palette.text.primary,
-            }}>
-            {t("no_account")}{" "}
-            <Link
-              variant="body2"
-              sx={{ color: theme.colors.primary, cursor: "pointer" }}
-              onClick={handleSignUpClick}>
-              {t("signup_link")}
-            </Link>
-          </Typography>
-        </Box>
+            sx={{ color: theme.colors.primary, cursor: "pointer" }}
+            onClick={handleSignUpClick}>
+            {t("signup_link")}
+          </Link>
+        </Typography>
       </Box>
+      <InfoModal
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        dialogMessage={successMessage}
+        userName={userName}
+        userSurname={userSurname}
+      />
     </Container>
   );
 }
---
+++ FrontEnd/src/components/forms/auth/SignInForm.js
@@ -0,0 +1,62 @@
+// src/components/forms/auth/SignInForm.js
+import React from "react";
+import { Box, Typography, Link } from "@mui/material";
+import CustomTextField from "../../customComponents/CustomTextField";
+import CustomButton from "../../customComponents/CustomButton";
+import { pxToRem } from "../../../utils/pxToRem";
+
+const SignInForm = ({
+  formData,
+  handleInputChange,
+  handleSignIn,
+  error,
+  t,
+  mfaRequired,
+}) => {
+  return (
+    <Box component="form" noValidate onSubmit={handleSignIn}>
+      <CustomTextField
+        label={t("email_label")}
+        name="email"
+        autoFocus
+        onChange={handleInputChange}
+      />
+      <CustomTextField
+        label={t("password_label")}
+        name="password"
+        type="password"
+        onChange={handleInputChange}
+      />
+
+      {/* Mostra il campo MFA solo se richiesto */}
+      {mfaRequired && (
+        <CustomTextField
+          label={t("mfa_code_label")}
+          name="mfaToken"
+          type="text"
+          onChange={handleInputChange}
+        />
+      )}
+
+      {/* Messaggio di errore */}
+      {error && (
+        <Typography color="error" variant="body2" sx={{ mt: 2 }}>
+          {error}
+        </Typography>
+      )}
+
+      {/* Bottone per inviare il form */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: pxToRem(16),
+          mt: pxToRem(8),
+        }}>
+        <CustomButton type="submit">{t("signin_button")}</CustomButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default SignInForm;
